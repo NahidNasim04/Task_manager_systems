7aa4c873ef0d6b49e6378c918d3eeb86e8e70f36
NahidNasim04 <nahidnasim04@gmail.com>
2026-02-19 13:29:41 +0530

    added file

---
+++ frontend/src/components/Create.tsx
@@ -0,0 +1,102 @@
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+/******
+ Create task
+(title,
+description,
+priority,
+ Mark as
+completed
+deadline)
+ Edit task
+ Delete task
+ Filter
+(Completed
+/ Pending)
+https://698ec424aded595c2532b6b0.mockapi.io/task
+ */
+export const Create = () => {
+  const [title, SetTitle] = useState("");
+  const [description, SetDescription] = useState("");
+  const [priority, SetPriority] = useState("");
+  const [mark, SetMark] = useState("");
+  const [deadline, SetDeadline] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>)=> {
+    e.preventDefault();
+    axios
+      .post("https://698ec424aded595c2532b6b0.mockapi.io/task", {
+        title,
+        description,
+        priority,
+        mark,
+        deadline,
+      })
+      .then((res) => {
+        navigate("/");
+        console.log("res", res);
+        
+      })
+      .catch((err) => {
+        console.log("error", err);
+      });
+  };
+
+  return (
+    <>
+      <div>
+        <h1>Create Task Manager</h1>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="title">Title</label>
+            <input
+              type="text"
+              value={title}
+              placeholder="Title name"
+              onChange={(e) => SetTitle(e.target.value)}
+            ></input>
+          </div>
+          <div>
+            <label htmlFor="description">Description</label>
+            <input
+              type="text"
+              value={description}
+              placeholder="Description"
+              onChange={(e) => SetDescription(e.target.value)}
+            ></input>
+          </div>
+          <div>
+            <label htmlFor="priority">Priority</label>
+            <input
+              type="text"
+              value={priority}
+              placeholder="Priority"
+              onChange={(e) => SetPriority(e.target.value)}
+            ></input>
+          </div>
+          <div>
+            <label htmlFor="title">Mark as complited</label>
+            <select value={mark} onChange={(e) => SetMark(e.target.value)}>
+              <option value="completed">Completed</option>
+              <option value="pending">Pending</option>
+              <option value="on going">On going</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="deadline">Deadline</label>
+            <input
+              type="date"
+              value={deadline}
+              placeholder="Deadline date"
+              onChange={(e) => SetDeadline(e.target.value)}
+            ></input>
+          </div>
+          <input type="submit" value="Submit"></input>
+        </form>
+      </div>
+    </>
+  );
+};
---
+++ frontend/src/components/Read.tsx
@@ -0,0 +1,133 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+
+interface DataItem {
+  id: number;
+  title: string;
+  description: string;
+  priority: string;
+  mark: string;
+  deadline: string;
+}
+
+export const Read = () => {
+  const [apiData, setApiData] = useState<DataItem[]>([]);
+ const navigate = useNavigate();
+
+  function getData() {
+    axios
+      .get("https://698ec424aded595c2532b6b0.mockapi.io/task")
+
+      .then((res) => {
+        setApiData(res.data);
+      })
+      .catch((err) => {
+        console.log("Error", err);
+      });
+  }
+  function handleDelete(id: number | string) {
+    axios
+      .delete(`https://698ec424aded595c2532b6b0.mockapi.io/task/${id}`)
+      .then(() => {
+        getData();
+      })
+      .catch((err) => {
+        console.log("Error", err);
+      });
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  function setDataToStorage(
+    id: number | string,
+    title: string,
+    description: string,
+    priority: string,
+    mark: string,
+    deadline: string,
+  ) {
+    localStorage.setItem("id", id.toString());
+    localStorage.setItem("title", title);
+    localStorage.setItem("description", description);
+    localStorage.setItem("priority", priority);
+    localStorage.setItem("mark", mark);
+    localStorage.setItem("deadline", deadline);
+  }
+
+  return (
+    <>
+      <div>
+        <h1>Crud Read</h1>
+      </div>
+      <Link to="/create">
+        <button>Create</button>
+        <div>
+          <div>
+            <table>
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>TITLE</th>
+                  <th>DESCRIPTION</th>
+                  <th>PRIORITY</th>
+                  <th>MARK</th>
+                  <th>DEADLINE</th>
+                  <th>Edit</th>
+                  <th>Delete</th>
+                </tr>
+              </thead>
+              <tbody>
+                {apiData.map((data) => {
+                  return (
+                    <tr key={data.id}>
+                      <td>{data.title}</td>
+                      <td>{data.description}</td>
+                      <td>{data.priority}</td>
+                      <td>{data.mark}</td>
+                      <td>{data.deadline}</td>
+                      <td>
+                        <Link to="/Edit">
+                          <button
+                            onClick={() => {
+                              setDataToStorage(
+                                data.id,
+                                data.title,
+                                data.description,
+                                data.priority,
+                                data.mark,
+                                data.deadline,
+                              );
+                            }}
+                          >
+                            Edit
+                          </button>
+                        </Link>
+                      </td>
+                      <td>
+                        <button
+                          onClick={() => {
+                            if (
+                              window.confirm("Are you sure to delete data??")
+                            ) {
+                              handleDelete(data.id);
+                              navigate("/");
+                            }
+                          }}
+                        >
+                          Delete
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </Link>
+    </>
+  );
+};
